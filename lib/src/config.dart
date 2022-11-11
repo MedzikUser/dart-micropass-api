03d447e4066a5bc548b8e80410936359e355d637
MedzikUser <medzik@duck.com>
2022-11-11 19:34:09 +0100

    fix: api prefix url

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,5 +1,5 @@
 class Config {
   static const int masterPasswordIterations = 100000;
-  static const String apiDomain = "micropass-api.medzik.xyz";
-  static const String apiPrefix = "/";
+  static const String apiDomain = 'micropass-api.medzik.xyz';
+  static const String apiPrefix = '';
 }
